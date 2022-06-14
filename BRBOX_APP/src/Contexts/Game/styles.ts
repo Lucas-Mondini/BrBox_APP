cee2205a76572ccb461867b77e6f939c638ab3e8
JeanCDev <jeangames15@gmail.com>
2022-06-13 21:05:42 -0300

    Contexto para gerenciar dados do jogo

--- BRBOX_APP/src/Contexts/Game/styles.ts
+++ BRBOX_APP/src/Contexts/Game/styles.ts
@@ -3,17 +3,20 @@ import { StyleSheet } from "react-native";
 import config from "../../../brbox.config.json";
 
 const styles = StyleSheet.create({
-  container: {
-    height: "100%",
-    paddingHorizontal: "3%",
-    paddingTop: "10%"
+  linkContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10
   },
-  title: {
-    fontSize: 20,
+  xButton: {
+    position: "absolute",
+    right: 0
+  },
+  linkText: {
+    fontSize: 15,
     fontFamily: config.fontFamilyBold,
-    marginBottom:30,
-    textAlign: "center"
-  }
+  },
+
 });
 
 export default styles
