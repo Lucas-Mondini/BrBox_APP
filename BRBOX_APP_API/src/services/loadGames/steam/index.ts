87a84a6b7bd4bbe747233a925f328099bea60e64
Lucas-mondini <lucasfepereira1@gmail.com>
2022-06-24 21:02:58 -0300

    changing the game initialization route and the login ORM method

--- BRBOX_APP_API/src/services/loadGames/steam/index.ts
+++ BRBOX_APP_API/src/services/loadGames/steam/index.ts
@@ -14,7 +14,7 @@ export default class SteamLoader {
     games : any
     
     constructor() {
-        this.games = require('../../../../games.json')
+        this.games = require('../../../../games-heroku.json')
     }
     loadFromJSON = async () => {
         let counter = 0
