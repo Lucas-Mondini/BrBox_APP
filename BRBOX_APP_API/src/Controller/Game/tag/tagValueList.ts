52baf6fe4b179b951f5a86b3a9db0b77575f8124
Lucas Mondini <lucas.pereira@neomind.com.br>
2022-07-28 22:03:43 -0300

    changing the return object to remove the payload of the frontend

--- BRBOX_APP_API/src/Controller/Game/tag/tagValueList.ts
+++ BRBOX_APP_API/src/Controller/Game/tag/tagValueList.ts
@@ -9,6 +9,42 @@ import Value from "../../../Model/Game/tag/value";
 import GameTime from "../../../Model/Game/gameTime";
 import { weightCalculator } from "../../../Utils/calculator";
 
+
+/**
+   * Receive a list of evaluated tags and returns a list of tags grouped by tag
+   * @param list
+   * @return Tag[]
+   */
+ function groupEvaluatedTags(list: any[]): any[]
+ {
+   const finalValues: any[] = [];
+
+   for (let item1 of list) {
+     const countTags = list.filter((item) => item.tag.id === item1.tag.id);
+     const countUpVotes = list.filter((item) => item.value.id === 1 && item.tag.id === item1.tag.id).length;
+     const countDownVotes = list.filter((item) => item.value.id === 3 && item.tag.id === item1.tag.id).length;
+     const countNeutralVotes = list.filter((item) => item.value.id === 2 && item.tag.id === item1.tag.id).length;
+
+     finalValues.push({
+       id: countTags[0].tag.id,
+       icon: countTags[0].tag.icon,
+       count: countTags.length,
+       upVotes: countUpVotes,
+       neutralVotes: countNeutralVotes,
+       downVotes: countDownVotes,
+       evalId: countTags[0].id,
+       name: countTags[0].tag.name,
+       value: countTags[0].value.id,
+       description_positive: countTags[0].tag.description_positive,
+       description_neutral: countTags[0].tag.description_neutral,
+       description_negative: countTags[0].tag.description_negative
+     });
+   }
+
+   return [
+     ...new Map(finalValues.map((item) => [item["id"], item])).values(),
+   ];
+ }
 export default class TagValueListController extends Controller {
     constructor() {
         super(TagValueList, ["tagValues", "tagValues.tag", "tagValues.user", "tagValues.value"])
@@ -22,7 +58,7 @@ export default class TagValueListController extends Controller {
 
             const tagValueFromUser = tagValue.tagValues.filter(i => i.user.id == req.user.id)
             
-            return {status: 200, value: {tagValueFromUser: tagValueFromUser, tagValue: tagValue}};
+            return {status: 200, value: {tagValueFromUser: groupEvaluatedTags(tagValueFromUser), tagValue: groupEvaluatedTags(tagValue.tagValues)}};
         }
          catch (e : any) {
             return {status: 500, value: {message: {"something went wrong" : (e.detail || e.message || e)}}};
